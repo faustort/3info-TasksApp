e883d7fac85cfa5187d05e54f51f2c01faf18e17
Fausto <fausto.toloi@prof.sc.senac.br>
2023-07-05 14:00:36 -0300

    comit inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import LoadingScreen from "./screens/LoadingScreen";
+
+const { createNativeStackNavigator } = require("@react-navigation/native-stack");
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
---
+++ src/utils/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    selfFullWidth: {
+        alignSelf: "stretch",
+    }
+})
